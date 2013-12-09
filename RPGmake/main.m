37b10c70b8d071e279e04c574e590a2ad2d4f6d9
Paul Park <paulpark@Pauls-MacBook-Air.local>
2013-12-08 19:13:18 -0500

    Initial Commit

---
+++ RPGmake/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RPGmake
+//
+//  Created by Paul Park on 12/8/13.
+//  Copyright (c) 2013 Paul Park. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
